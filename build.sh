8effd1d47ba6cf5dd7d8000ec4011ef3ec0043b1
Sune <sune@suneweb.net>
2022-08-02 22:35:28 +0200

    Moved Docker files to docker directory. Builds are outputted to build directory and a subdirectory specifying the OS.

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker run --rm -v "$(pwd):/workdir" -e CROSS_TRIPLE=x86_64-w64-mingw32  multiarch/crossbuild bash -c "mkdir -p build/win && cd build/win && cmake ../.. && make"
-docker run --rm -v "$(pwd):/workdir" -e CROSS_TRIPLE=x86_64-linux-gnu    multiarch/crossbuild bash -c "mkdir -p build/unx && cd build/unx && cmake ../.. && make"
-docker run --rm -v "$(pwd):/workdir" -e CROSS_TRIPLE=x86_64-apple-darwin multiarch/crossbuild bash -c "mkdir -p build/osx && cd build/osx && cmake ../.. && make"
+docker run --rm -v "$(pwd):/workdir" -e CROSS_TRIPLE=x86_64-w64-mingw32  multiarch/crossbuild bash -c "mkdir -p build/windows-x64 && cd build/windows-x64 && cmake ../.. && make && mv mdrun mdrun.exe"
+docker run --rm -v "$(pwd):/workdir" -e CROSS_TRIPLE=x86_64-linux-gnu    multiarch/crossbuild bash -c "mkdir -p build/linux-x64   && cd build/linux-x64   && cmake ../.. && make"
+docker run --rm -v "$(pwd):/workdir" -e CROSS_TRIPLE=x86_64-apple-darwin multiarch/crossbuild bash -c "mkdir -p build/osx-x64     && cd build/osx-x64     && cmake ../.. && make"
